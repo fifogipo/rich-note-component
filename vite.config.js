45ee8663a3da287c5e125f569c41c489e41813a1
Federico Gasperoni <f.gasperoni@xister.com>
2025-04-06 21:43:54 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/rich-note/rich-note.component.ts'),
-            name: 'RichNote',
-            fileName: (format) => `rich-note.${format}.js`,
-            formats: ['es']
-        },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/rich-note/rich-note.component.ts"),
+      name: "RichNote",
+      fileName: (format) => `rich-note.${format}.js`,
+      formats: ["es"],
     },
-    plugins: [dts()]
+    rollupOptions: {
+      external: ["lit", "lit/directives/unsafe-html.js", "lit/directives/class-map.js"],
+      output: {
+        globals: {
+          lit: "Lit",
+        },
+      },
+    },
+  },
 });
